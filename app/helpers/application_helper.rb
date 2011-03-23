bf0f952eacfed615a917e76424ad85dbe00c58f7
Jacob <jacob.krehbiel@gmail.com>
2011-03-23 14:26:47 -0400

    Added a title helper and style sheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
   # Return a title on a per-page basis
   def title
-    base_file = "Ruby on Rails Tutoral Sample App"
+    base_title = "Ruby on Rails Tutoral Sample App"
     if @title.nil?
       base_title
     else
-      "#{base_title}" | #{@title}
+      "#{base_title} | #{@title}"
     end
   end
 end
